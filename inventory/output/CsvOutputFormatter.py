1d17b38d6854a5f4b19c5e57cc1e63541e8e7aca
Hervé PRECIGOUT <hprecigout@free.fr>
2024-05-28 17:15:39 +0200

    Supression des resultats d'inventaires

--- inventory/output/CsvOutputFormatter.py
+++ inventory/output/CsvOutputFormatter.py
@@ -17,17 +17,17 @@ class CsvOutputFormatter(OutputFormatter):
         for resource_key, resource in self._resources.items():
             if not printed_headers:
                 header_line = ""
-                for my_field_key, my_filed_value in resource.Data().items():
+                for my_field_key, my_field_value in resource.Data().items():
                     if header_line != "":
                         header_line += self._config['csv_separator']
                     header_line += f"\"{my_field_key}\""
                 output += f"{header_line}\n"
                 printed_headers = True
             resource_line = ""
-            for my_field_key, my_filed_value in resource.Data().items():
+            for my_field_key, my_field_value in resource.Data().items():
                 if resource_line != "":
                     resource_line += self._config['csv_separator']
-                resource_line += f"\"{my_filed_value}\""
+                resource_line += f"\"{my_field_value}\""
             output += f"{resource_line}\n"
             
         return output
