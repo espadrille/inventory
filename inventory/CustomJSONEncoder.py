12d1327f00acd60c6589755629329d3a738983e0
Hervé PRECIGOUT <hyper.poubelle@gmail.com>
2024-11-14 23:20:38 +0100

    Mise en forme

--- inventory/CustomJSONEncoder.py
+++ inventory/CustomJSONEncoder.py
@@ -19,13 +19,13 @@ class CustomJSONEncoder(JSONEncoder):
         if isinstance(o, set):
             return list(o)
 
-        # Gestion des objets personnalisés avec '__dict__'
+        # Gestion des objets personnalises avec '__dict__'
         if hasattr(o, '__dict__'):
             try:
-                # Sérialiser seulement les attributs publics
+                # Serialiser seulement les attributs publics
                 return {k: v for k, v in o.__dict__.items() if not k.startswith('_')}
             except Exception:
                 return str(o)
 
-        # Par défaut, appeler le comportement de la superclasse
+        # Par defaut, appeler le comportement de la superclasse
         return super().default(o)
