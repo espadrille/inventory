16f97ae09f854eb48cd855895e57891fd98ead13
Hervé PRECIGOUT <hyper.poubelle@gmail.com>
2024-11-14 07:38:54 +0100

    Mise en forme

--- inventory/CustomJSONEncoder.py
+++ inventory/CustomJSONEncoder.py
@@ -6,11 +6,9 @@ from json import JSONEncoder
 class CustomJSONEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime.datetime):
-            return f"{o}"
+            return str(o)
         if hasattr(o, '__dict__'):
             try:
                 return o.__dict__
-            except:
+            except Exception:
                 return {k: v for k, v in o.__dict__.items() if not k.startswith('_')}
-                
-
